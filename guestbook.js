fd2ca8d547ac21b7a862560ade25520e0c8336cd
rashidreyaz <30404532+rashidreyaz@users.noreply.github.com>
2021-05-13 16:16:51 +0530

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://5e2308c9.us-south.apigw.appdomain.cloud/guestbook/';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
